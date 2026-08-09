acfb48a2c6af1fcc88a5a53d43ff286c9f57d0e8
Ibrahim Abdulkadir Mohamed <ibrahimabdulkadir2519@gmail.com>
2026-08-09 14:24:50 +0300

    Add TypeScript exercises #1

---
+++ Exercises-1/exercise-1.js
@@ -0,0 +1,34 @@
+"use strict";
+// const ProductName : string = "iPhone 15";
+// const Price : number = 1000;
+// const discountAvailable : boolean = true;
+// error: cannot reassign a value to a constant
+// ProductName  = 100;
+// Price = "500";
+// discountAvailable = "yes";
+// correct way to assign a value to a constant
+// ProductName = "iPhone 16";
+// Price = 1500;
+// discountAvailable = false;
+// console.log(`Product Name: ${ProductName}`);
+// console.log(`Price: ${Price}`);
+// console.log(`Discount Available: ${discountAvailable}`);
+// function getDiscount(price: number, discount: number): number {
+//     return price - price * discount;
+//   }
+//   console.log(getDiscount(100, 0.2));
+// ### 3. ⚠️ Dangerous `any`
+// Here’s a risky TypeScript function using `any`:
+// // ✅ Replace `any` with a safer approach and prevent the crash.
+// function printLength(x: any) {
+//     console.log(x.length);
+//   }
+//   printLength("Hello");
+//   printLength(123);
+function printLength(x) {
+    if (typeof x === 'string') {
+        console.log(x.length);
+    }
+}
+//   printLength("Hello"); // should work
+printLength(123); // should crash
